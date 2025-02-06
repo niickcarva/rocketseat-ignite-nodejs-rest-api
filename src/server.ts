ab3025b49cc23f28489fc4854b91988e44a32e6a
niickcarva <nicolasc.avelaneda@gmail.com>
2025-02-06 12:47:57 -0300

    add global hook

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
 
+app.addHook("preHandler", async (request) => {
+  console.log(`👀 [${request.method}] ${request.url}`);
+});
+
 app.register(cookie);
 app.register(transactionsRoutes, {
   prefix: "/transactions",
